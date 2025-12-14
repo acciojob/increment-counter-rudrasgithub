a770edf26817fc4bd80fc66b9e84e1a7c810dac9
Pasupuleti Rudrama Naidu <121972044+rudrasgithub@users.noreply.github.com>
2025-12-14 22:45:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let counter = document.getElementById("counter");
+function Increment() {
+	let value = counter.textContent;
+	value += 1
+	counter.textContent = value;
+}
+alert(counter.textContent)
