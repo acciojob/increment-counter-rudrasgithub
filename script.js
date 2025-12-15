c785ee44e6977e744306f9439f1e35ff5dc29bb1
Pasupuleti Rudrama Naidu <121972044+rudrasgithub@users.noreply.github.com>
2025-12-15 13:43:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //your JS code here. If required.
 let counter = document.getElementById("counter");
 function Increment() {
+	alert(counter.textContent)
 	let value = Number(counter.textContent);
 	value += 1
 	counter.textContent = value;
-	alert(counter.textContent)
 }
